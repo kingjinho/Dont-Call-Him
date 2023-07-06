bb88c58504df4b6b839495350510ee26598e98e9
kingjinho <jinhoshin319@gmail.com>
2023-07-06 21:22:34 +0900

    feat: write a happy path test regarding main screen navigation

---
+++ app/src/androidTest/java/com/kingjinho/dontcallhim/MainScreenTest.kt
@@ -0,0 +1,37 @@
+package com.kingjinho.dontcallhim
+
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.navigation.Navigation
+import androidx.navigation.testing.TestNavHostController
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.google.common.truth.Truth.assertThat
+import com.kingjinho.dontcallhim.screen.ScreenMain
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+@RunWith(AndroidJUnit4::class)
+class MainScreenTest {
+
+    @Test
+    fun testNavigationToAddRemoveNumberScreen() {
+        val navController = TestNavHostController(
+            ApplicationProvider.getApplicationContext()
+        )
+
+        val mainScenario = launchFragmentInContainer<ScreenMain>()
+
+        mainScenario.onFragment {
+            navController.setGraph(R.navigation.nav_graph)
+            Navigation.setViewNavController(it.requireView(), navController)
+        }
+
+        onView(ViewMatchers.withId(R.id.to_add_number)).perform(ViewActions.click())
+
+        assertThat(navController.currentDestination?.id).isEqualTo(R.id.screenAddRemoveNumber)
+    }
+}
