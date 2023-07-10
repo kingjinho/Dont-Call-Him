6e6574a0a39b6990cd352cbd4321c0b8dd3f1296
kingjinho <jinhoshin319@gmail.com>
2023-07-10 16:57:28 +0900

    refactor: changes in package of screens

--- app/src/androidTest/java/com/kingjinho/dontcallhim/NavigationTest.kt
+++ app/src/androidTest/java/com/kingjinho/dontcallhim/NavigationTest.kt
@@ -9,7 +9,7 @@ import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.google.common.truth.Truth.assertThat
-import com.kingjinho.dontcallhim.screen.ScreenMain
+import com.kingjinho.dontcallhim.screen.main.ScreenMain
 import org.junit.Test
 import org.junit.runner.RunWith
 
