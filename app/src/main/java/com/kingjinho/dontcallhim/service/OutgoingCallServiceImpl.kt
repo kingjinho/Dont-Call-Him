47fb7a19de0282fe2bb4c786a767b8bbc5317993
kingjinho <jinhoshin319@gmail.com>
2023-07-10 16:59:46 +0900

    feat: add call placement logic

--- app/src/main/java/com/kingjinho/dontcallhim/service/OutgoingCallServiceImpl.kt
+++ app/src/main/java/com/kingjinho/dontcallhim/service/OutgoingCallServiceImpl.kt
@@ -1,19 +1,31 @@
 package com.kingjinho.dontcallhim.service
 
 import android.net.Uri
-import android.os.Build
 import android.telecom.CallRedirectionService
 import android.telecom.PhoneAccountHandle
-import androidx.annotation.RequiresApi
+import com.kingjinho.dontcallhim.DontCallHimApplication
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class OutgoingCallServiceImpl : CallRedirectionService() {
+
+    private val dbInstance by lazy {
+        (application as DontCallHimApplication).dbInstance
+    }
 
     override fun onPlaceCall(
         handle: Uri,
         initialPhoneAccount: PhoneAccountHandle,
         allowInteractiveResponse: Boolean
     ) {
-        cancelCall()
+        CoroutineScope(Dispatchers.Main).launch {
+            if (dbInstance.phoneNumberDao().getExistingData(handle.schemeSpecificPart)) {
+                cancelCall()
+            } else {
+                placeCallUnmodified()
+            }
+        }
     }
 
 }
