1e31a58b036eedd3c6e16cd70b9fe4e6bf5d3ed0
kingjinho <jinhoshin319@gmail.com>
2023-07-17 14:56:57 +0900

    refactor: use Dagger

--- app/src/main/java/com/kingjinho/dontcallhim/DontCallHimApplication.kt
+++ app/src/main/java/com/kingjinho/dontcallhim/DontCallHimApplication.kt
@@ -2,11 +2,16 @@ package com.kingjinho.dontcallhim
 
 import android.app.Application
 import com.kingjinho.dontcallhim.db.AppDatabase
+import com.kingjinho.dontcallhim.di.application.AppComponent
+import com.kingjinho.dontcallhim.di.application.AppModule
+import com.kingjinho.dontcallhim.di.application.DaggerAppComponent
 
-class DontCallHimApplication: Application() {
+class DontCallHimApplication : Application() {
 
-    val dbInstance: AppDatabase by lazy {
-        AppDatabase.getInstance(applicationContext)
+    val appComponent: AppComponent by lazy {
+        DaggerAppComponent.builder()
+            .appModule(AppModule(this))
+            .build()
     }
 
 }
