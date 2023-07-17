f9ef085f389cfed71b0f838f804d35f180e0b8a8
kingjinho <jinhoshin319@gmail.com>
2023-07-17 14:58:37 +0900

    refactor: use Dagger to construct viewmodel

--- app/src/main/java/com/kingjinho/dontcallhim/screen/add/ScreenAddNumber.kt
+++ app/src/main/java/com/kingjinho/dontcallhim/screen/add/ScreenAddNumber.kt
@@ -8,15 +8,9 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
-import com.kingjinho.dontcallhim.DontCallHimApplication
 import com.kingjinho.dontcallhim.R
-import com.kingjinho.dontcallhim.repository.AddNumberRepo
-import com.kingjinho.dontcallhim.repository.FetchNumbersRepo
-import com.kingjinho.dontcallhim.usecase.add.AddNumberUseCaseImpl
-import com.kingjinho.dontcallhim.usecase.fetch.FetchNumbersUseCaseImpl
+import com.kingjinho.dontcallhim.utils.getAppComponent
 import com.kingjinho.dontcallhim.viewmodels.OutgoingCallVM
 import com.kingjinho.dontcallhim.viewmodels.OutgoingCallVMFactory
 import com.kingjinho.dontcallhim.viewmodels.Result
@@ -28,18 +22,11 @@ class ScreenAddNumber : Fragment(), ScreenAddNumberMvc.Listener {
 
     private lateinit var viewMvc: ScreenAddNumberMvc
     private var fetchAddNumbersJob: Job? = null
+
     private val addNumberVM by viewModels<OutgoingCallVM> {
         OutgoingCallVMFactory(
-            addNumberUseCase = AddNumberUseCaseImpl(
-                addNumberRepo = AddNumberRepo(
-                    db = (requireActivity().application as DontCallHimApplication).dbInstance
-                )
-            ),
-            fetchNumbersUseCase = FetchNumbersUseCaseImpl(
-                fetchNumbersRepo = FetchNumbersRepo(
-                    db = (requireActivity().application as DontCallHimApplication).dbInstance
-                )
-            )
+            addNumberUseCase = getAppComponent().newPresentationComponent().getAddNumberUseCase(),
+            fetchNumbersUseCase = getAppComponent().newPresentationComponent().getFetchNumbersUseCase()
         )
      }
 
