523eb374798a5534e37ea50ff5efed2e4efe5f16
kingjinho <jinhoshin319@gmail.com>
2023-07-18 13:33:00 +0900

    feat: add base view mvc factory

--- app/src/main/java/com/kingjinho/dontcallhim/di/presentation/PresentationModule.kt
+++ app/src/main/java/com/kingjinho/dontcallhim/di/presentation/PresentationModule.kt
@@ -1,8 +1,10 @@
 package com.kingjinho.dontcallhim.di.presentation
 
+import android.view.LayoutInflater
 import com.kingjinho.dontcallhim.db.AppDatabase
 import com.kingjinho.dontcallhim.repository.AddNumberRepo
 import com.kingjinho.dontcallhim.repository.FetchNumbersRepo
+import com.kingjinho.dontcallhim.screen.viewfactory.BaseViewMvcFactory
 import com.kingjinho.dontcallhim.usecase.add.AddNumberUseCase
 import com.kingjinho.dontcallhim.usecase.add.AddNumberUseCaseImpl
 import com.kingjinho.dontcallhim.usecase.fetch.FetchNumbersUseCase
@@ -12,6 +14,10 @@ import dagger.Provides
 
 @Module
 class PresentationModule {
+
+    @Provides
+    fun getBaseViewMvcFactory(layoutInflater: LayoutInflater): BaseViewMvcFactory =
+        BaseViewMvcFactory(layoutInflater)
 
     @Provides
     fun getAddNumberRepo(db: AppDatabase): AddNumberRepo = AddNumberRepo(db)
