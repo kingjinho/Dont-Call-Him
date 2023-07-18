c3916e0d8dd3e22210327d38c3255220fe06a519
kingjinho <jinhoshin319@gmail.com>
2023-07-18 13:29:46 +0900

    remove: unnecessary

--- app/src/main/java/com/kingjinho/dontcallhim/utils/DaggerComponentExt.kt
+++
@@ -1,8 +0,0 @@
-package com.kingjinho.dontcallhim.utils
-
-import androidx.fragment.app.Fragment
-import com.kingjinho.dontcallhim.DontCallHimApplication
-import com.kingjinho.dontcallhim.di.application.AppComponent
-
-fun Fragment.getAppComponent(): AppComponent =
-    (requireActivity().application as DontCallHimApplication).appComponent
