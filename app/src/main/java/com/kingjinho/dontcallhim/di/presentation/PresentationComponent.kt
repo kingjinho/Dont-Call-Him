aa0bc4b9a1b3551e7b06c3265fe885307fece639
kingjinho <jinhoshin319@gmail.com>
2023-07-18 13:33:53 +0900

    feat: add screen level component

--- app/src/main/java/com/kingjinho/dontcallhim/di/presentation/PresentationComponent.kt
+++ app/src/main/java/com/kingjinho/dontcallhim/di/presentation/PresentationComponent.kt
@@ -1,15 +1,12 @@
 package com.kingjinho.dontcallhim.di.presentation
 
-import com.kingjinho.dontcallhim.usecase.add.AddNumberUseCase
-import com.kingjinho.dontcallhim.usecase.fetch.FetchNumbersUseCase
+import com.kingjinho.dontcallhim.screen.add.ScreenAddNumber
 import dagger.Subcomponent
 
 
 @Subcomponent(modules = [PresentationModule::class])
 interface PresentationComponent {
 
-    fun getAddNumberUseCase(): AddNumberUseCase
-
-    fun getFetchNumbersUseCase(): FetchNumbersUseCase
+    fun inject(fragment: ScreenAddNumber)
 
 }
