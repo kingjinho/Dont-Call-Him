b1283fba0a787b54cd100b8bf70a15f5989973ac
kingjinho <jinhoshin319@gmail.com>
2023-07-20 12:20:25 +0900

    feat: add service component initialization function

--- app/src/main/java/com/kingjinho/dontcallhim/di/application/AppComponent.kt
+++ app/src/main/java/com/kingjinho/dontcallhim/di/application/AppComponent.kt
@@ -1,16 +1,16 @@
 package com.kingjinho.dontcallhim.di.application
 
-import com.kingjinho.dontcallhim.db.AppDatabase
 import com.kingjinho.dontcallhim.di.activity.ActivityComponent
 import com.kingjinho.dontcallhim.di.activity.ActivityModule
 import com.kingjinho.dontcallhim.di.scope.AppScope
+import com.kingjinho.dontcallhim.di.service.ServiceComponent
 import dagger.Component
 
 @AppScope
 @Component(modules = [AppModule::class])
 interface AppComponent {
 
-    fun getDatabase(): AppDatabase
-
     fun newActivityComponent(activityModule: ActivityModule): ActivityComponent
+
+    fun newServiceComponent(): ServiceComponent
 }
