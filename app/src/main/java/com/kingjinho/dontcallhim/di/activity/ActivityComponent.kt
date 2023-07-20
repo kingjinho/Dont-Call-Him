1af8142d7e64e2bc066b92b5f1c43d3975c9db09
kingjinho <jinhoshin319@gmail.com>
2023-07-20 16:13:46 +0900

    feat: add builder interface

--- app/src/main/java/com/kingjinho/dontcallhim/di/activity/ActivityComponent.kt
+++ app/src/main/java/com/kingjinho/dontcallhim/di/activity/ActivityComponent.kt
@@ -1,9 +1,10 @@
 package com.kingjinho.dontcallhim.di.activity
 
+import androidx.appcompat.app.AppCompatActivity
 import com.kingjinho.dontcallhim.di.presentation.PresentationComponent
 import com.kingjinho.dontcallhim.di.presentation.PresentationModule
 import com.kingjinho.dontcallhim.di.scope.ActivityScope
-import dagger.Component
+import dagger.BindsInstance
 import dagger.Subcomponent
 
 @Subcomponent(modules = [ActivityModule::class])
@@ -11,4 +12,13 @@ import dagger.Subcomponent
 interface ActivityComponent {
 
     fun newPresentationComponent(presentationModule: PresentationModule): PresentationComponent
+
+    @Subcomponent.Builder
+    interface Builder {
+
+        @BindsInstance
+        fun activity(activity: AppCompatActivity): Builder
+        fun activityModule(activityModule: ActivityModule): Builder
+        fun build(): ActivityComponent
+    }
 }
