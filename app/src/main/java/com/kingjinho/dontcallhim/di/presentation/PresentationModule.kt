0d811048aaf34723a9a6f883f52dbf5a91f8d58a
kingjinho <jinhoshin319@gmail.com>
2023-07-20 12:19:49 +0900

    refactor: remove redundant dependency due to constructor injection

--- app/src/main/java/com/kingjinho/dontcallhim/di/presentation/PresentationModule.kt
+++ app/src/main/java/com/kingjinho/dontcallhim/di/presentation/PresentationModule.kt
@@ -1,10 +1,7 @@
 package com.kingjinho.dontcallhim.di.presentation
 
-import android.view.LayoutInflater
-import com.kingjinho.dontcallhim.db.AppDatabase
 import com.kingjinho.dontcallhim.repository.AddNumberRepo
 import com.kingjinho.dontcallhim.repository.FetchNumbersRepo
-import com.kingjinho.dontcallhim.screen.viewfactory.BaseViewMvcFactory
 import com.kingjinho.dontcallhim.usecase.add.AddNumberUseCase
 import com.kingjinho.dontcallhim.usecase.add.AddNumberUseCaseImpl
 import com.kingjinho.dontcallhim.usecase.fetch.FetchNumbersUseCase
@@ -14,16 +11,6 @@ import dagger.Provides
 
 @Module
 class PresentationModule {
-
-    @Provides
-    fun getBaseViewMvcFactory(layoutInflater: LayoutInflater): BaseViewMvcFactory =
-        BaseViewMvcFactory(layoutInflater)
-
-    @Provides
-    fun getAddNumberRepo(db: AppDatabase): AddNumberRepo = AddNumberRepo(db)
-
-    @Provides
-    fun getFetchNumbersRepo(db: AppDatabase): FetchNumbersRepo = FetchNumbersRepo(db)
 
     @Provides
     fun getAddNumberUseCase(repo: AddNumberRepo): AddNumberUseCase = AddNumberUseCaseImpl(repo)
