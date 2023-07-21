e8b95350b0a54628f3659a3a0b036ffd54e45d79
kingjinho <jinhoshin319@gmail.com>
2023-07-21 10:36:21 +0900

    refactor: removed factory

--- app/src/main/java/com/kingjinho/dontcallhim/viewmodels/OutgoingCallVM.kt
+++ app/src/main/java/com/kingjinho/dontcallhim/viewmodels/OutgoingCallVM.kt
@@ -1,34 +1,13 @@
 package com.kingjinho.dontcallhim.viewmodels
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
 import com.kingjinho.dontcallhim.usecase.add.AddNumberUseCase
 import com.kingjinho.dontcallhim.usecase.fetch.FetchNumbersUseCase
 import com.kingjinho.dontcallhim.utils.isValidPhoneNumber
 import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
-import javax.inject.Provider
 
-class OutgoingCallVMFactory @Inject constructor(
-    private val addNumberUseCaseProvider: Provider<AddNumberUseCase>,
-    private val fetchNumbersUseCaseProvider: Provider<FetchNumbersUseCase>
-) : ViewModelProvider.Factory {
-
-    override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        @Suppress("UNCHECKED_CAST")
-        return if (modelClass.isAssignableFrom(OutgoingCallVM::class.java)) {
-            OutgoingCallVM(
-                addNumberUseCase = addNumberUseCaseProvider.get(),
-                fetchNumbersUseCase = fetchNumbersUseCaseProvider.get()
-            ) as T
-        } else {
-            throw IllegalArgumentException("modelClass is not assignable: ${modelClass.canonicalName}")
-        }
-    }
-
-}
-
-class OutgoingCallVM(
+class OutgoingCallVM @Inject constructor(
     private val addNumberUseCase: AddNumberUseCase,
     private val fetchNumbersUseCase: FetchNumbersUseCase
 ) : ViewModel() {
