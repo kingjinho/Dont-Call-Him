0cc62a21bf0e6924539fb384e2825206a2577698
kingjinho <jinhoshin319@gmail.com>
2023-08-03 16:49:15 +0900

    feat: add PhoneNumber Formatter for compose textfield

---
+++ app/src/main/java/com/kingjinho/dontcallhim/utils/PhoneNumberVisualTransformation.kt
@@ -0,0 +1,46 @@
+package com.kingjinho.dontcallhim.utils
+
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.input.OffsetMapping
+import androidx.compose.ui.text.input.TransformedText
+import androidx.compose.ui.text.input.VisualTransformation
+
+class PhoneNumberVisualTransformation : VisualTransformation {
+    override fun filter(text: AnnotatedString): TransformedText {
+        val annotatedString = AnnotatedString.Builder().apply {
+            val number = text.text
+
+            if (number.length < 3) {
+                append(number)
+            }
+
+            if (number.length in 3..7) {
+                append(number.substring(0, 3))
+                append("-")
+                append(number.substring(3))
+            }
+
+            if (number.length in 8..11) {
+                append(number.substring(0, 3))
+                append("-")
+                append(number.substring(3, 7))
+                append("-")
+                append(number.substring(7))
+            }
+
+        }.toAnnotatedString()
+
+        return TransformedText(
+            annotatedString,
+            offsetMapping = object : OffsetMapping {
+                override fun originalToTransformed(offset: Int): Int {
+                    return annotatedString.text.length
+                }
+
+                override fun transformedToOriginal(offset: Int): Int {
+                    return annotatedString.text.replace("-", "").length
+                }
+            }
+        )
+    }
+}
