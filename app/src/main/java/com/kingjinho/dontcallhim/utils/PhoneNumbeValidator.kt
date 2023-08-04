fa5f020afd1bb1f6e9220192a4fecb799ddf5cf2
kingjinho <jinhoshin319@gmail.com>
2023-08-04 16:21:36 +0900

    refactor: remove - for phone number regex

--- app/src/main/java/com/kingjinho/dontcallhim/utils/PhoneNumbeValidator.kt
+++ app/src/main/java/com/kingjinho/dontcallhim/utils/PhoneNumbeValidator.kt
@@ -2,7 +2,7 @@ package com.kingjinho.dontcallhim.utils
 
 import java.util.regex.Pattern
 
-private const val phoneNumberRegex = "^01([0-9])-(\\d{3}|\\d{4})-\\d{4}$"
+private const val phoneNumberRegex = "^01([0-9])(\\d{3}|\\d{4})\\d{4}$"
 private val pattern = Pattern.compile(phoneNumberRegex)
 
 fun String.isValidPhoneNumber(): Boolean {
