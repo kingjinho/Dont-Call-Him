1619a71cd0ecce207b0df476bf8f88ab5b547c5e
kingjinho <jinhoshin319@gmail.com>
2023-08-04 23:31:16 +0900

    feat: add property that displayed in UI

--- app/src/main/java/com/kingjinho/dontcallhim/db/entity/PhoneNumber.kt
+++ app/src/main/java/com/kingjinho/dontcallhim/db/entity/PhoneNumber.kt
@@ -1,12 +1,18 @@
 package com.kingjinho.dontcallhim.db.entity
 
+import android.telephony.PhoneNumberUtils
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.kingjinho.dontcallhim.utils.Constant
+import java.util.Locale
 
 @Entity(tableName = Constant.TABLE_NAME_PHONE_NUMBER)
 data class PhoneNumber(
 
     @PrimaryKey
     val number: String
-)
+) {
+    val formattedNumber: String
+        get() = PhoneNumberUtils.formatNumber(number, Locale.KOREA.country)
+
+}
