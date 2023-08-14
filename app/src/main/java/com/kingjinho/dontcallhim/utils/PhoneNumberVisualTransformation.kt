160779dd877fc6f5599741941c21f0e5d632cad2
kingjinho <jinhoshin319@gmail.com>
2023-08-14 17:44:46 +0900

    refactor: refactor phone number format conversion logic

--- app/src/main/java/com/kingjinho/dontcallhim/utils/PhoneNumberVisualTransformation.kt
+++ app/src/main/java/com/kingjinho/dontcallhim/utils/PhoneNumberVisualTransformation.kt
@@ -8,25 +8,33 @@ import androidx.compose.ui.text.input.VisualTransformation
 class PhoneNumberVisualTransformation : VisualTransformation {
     override fun filter(text: AnnotatedString): TransformedText {
         val annotatedString = AnnotatedString.Builder().apply {
+
             val number = text.text
 
             if (number.length < 3) {
                 append(number)
             }
 
-            if (number.length in 3..7) {
+            if (number.length >= 3) {
                 append(number.substring(0, 3))
                 append("-")
-                append(number.substring(3))
+                if (number.length < 7) {
+                    append(number.substring(3))
+                }
             }
 
-            if (number.length in 8..11) {
-                append(number.substring(0, 3))
-                append("-")
+            if (number.length >= 7) {
                 append(number.substring(3, 7))
                 append("-")
+                if (number.length < 11) {
+                    append(number.substring(7))
+                }
+            }
+
+            if (number.length >= 11) {
                 append(number.substring(7))
             }
+
 
         }.toAnnotatedString()
 
